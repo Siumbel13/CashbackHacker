98d9cd06e138938e833c2f7d2d761b82f56a9641
Siumbel <sumbelragipova@gmail.com>
2022-07-25 21:15:22 +0300

    добавление тестов

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,12 +1,27 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
-import static org.junit.Assert.*;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
 
     @Test
-    public void remain() {
+    public void remain1() {
+        int amount = 900;
+        int expected = 100;
+        int result = cashbackHackService.remain(amount);
+
+        assertEquals(result,expected);
+    }
+
+    @Test
+    public void remain2() {
+        int amount = 1000;
+        int expected = 0;
+        int result = cashbackHackService.remain(amount);
+
+        assertEquals(result,expected);
     }
 }
